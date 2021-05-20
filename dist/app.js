0044a6e5e87cd8707252df45969b63d1f9060a07
Stewart <stewart.mckinlay@gmail.com>
2021-05-20 16:42:00 -0600

    Implement functionality

--- dist/app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-console.log('test');
-console.log('test');
-console.log('test');
-//# sourceMappingURL=app.js.map
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,37 @@
-const path = require('path');
+const path = require("path");
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-	mode: 'development',
-	entry: './src/app.ts',
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist'),
-		publicPath: 'dist'
-	},
-	devtool: 'inline-source-map',
-	module: {
-		rules: [
-		{
-			test: /\.ts$/,
-			use: 'ts-loader',
-			exclude: /node_modules/
-		}
-		]
-	},
-	resolve: {
-		extensions: ['.ts', '.js']
-	}
-}
+  mode: "development",
+  entry: "./src/app.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist/",
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  devServer: {
+    open: true,
+    watchContentBase: true,
+    index: "index.html",
+    onListening: function (server) {
+      const port = server.listeningApp.address().port;
+      console.log("Listening on port:", port);
+    },
+  },
+  plugins: [
+    new Dotenv()
+  ]
+};
